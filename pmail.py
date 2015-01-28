98d382617ec4c04ee19a4ccaa9db3c81c6a92db5
lunaferie <wojt.ozog@gmail.com>
2015-01-28 16:30:19 +0100

    simple function to send email

--- pmail.py
+++ pmail.py
@@ -1,3 +1,37 @@
 #!/usr/bin/python
+# v1.0
+
+import smtplib
+
+# CONFIGURE:
+
+fromaddr = 'test@ayum.eu'
+toaddr = 'wojt.ozog@gmail.com'
+subject = 'test msg'
+
+text = 'This is a txt mesg from shire'
+
+server = 'localhost'
 
 
+#############################################
+
+def sendmail(fromaddr, toaddr, subject, text, server):
+    msg = "\r\n".join([
+        "From: %s" % fromaddr,
+	"To: %s" % toaddr,
+	"Subject: %s" % subject,
+	"", 
+	"%s" % text
+	])
+
+    try:
+	s = smtplib.SMTP(server)
+	s.sendmail(fromaddr, toaddr, msg)
+	s.quit()
+	print "INFO: message was sent"
+
+    except smtplib.SMTPException:
+	print "ERROR: unable to sent email"
+
+sendmail(fromaddr, toaddr, subject, text, server)
